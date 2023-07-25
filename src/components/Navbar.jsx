6f38c93eb40edd9cbf658ec75016227210c7ac06
Calvin Roberts <you@example.com>
2023-07-24 20:49:00 -0600

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,40 @@
-import React from 'react';
-function Navbar () {
-    return (
-        <div></div>
-    )
+import { ArrowRightIcon } from "@heroicons/react/solid";
+import React from "react";
+
+function Navbar() {
+  return (
+    <header className="bg-gray-800 md:sticky top-0 z-10">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="title-font font-medium text-white mb-4 md:mb-0">
+          <a href="#about" className="ml-3 text-xl">
+            Calvin Roberts
+          </a>
+        </a>
+
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-yellow-500	flex flex-wrap items-center text-base justify-center">
+          <a href="#about" className="mr-5 text-white hover:text-white">
+            About Me
+          </a>
+          <a href="#projects" className="mr-5 text-white hover:text-white">
+            Portfolio
+          </a>
+          <a href="#skills" className="mr-5 text-white hover:text-white">
+            Skills
+          </a>
+          <a href="#contact" className="mr-5 text-white hover:text-white">
+            Contact Me
+          </a>
+        </nav>
+        {/* <a
+          href="#contact"
+          className="inline-flex items-center bg-white border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+        >
+          Hire Me
+          <ArrowRightIcon className="w-4 h-4 ml-1" />
+        </a> */}
+      </div>
+    </header>
+  );
 }
 
 export default Navbar;
