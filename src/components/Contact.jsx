01da747d3f71db7995ccb4a6373d56026a924f24
Calvin Roberts <you@example.com>
2023-07-24 19:18:29 -0600

    updates to contact to fix margins

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,21 +17,21 @@ function Contact() {
           />
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+              <h2 className="title-font font-semibold text-yellow-500  tracking-widest text-xs">
                 ADDRESS
               </h2>
               <p className="mt-1 text-white">
                 350 State St, Salt Lake City, UT 84103
               </p>
             </div>
-            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+            <div className="lg:w-1/2 px-4 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-yellow-500 tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed ">
+              <a className="text-white leading-relaxed">
                 calvin.matsonroberts@outlook.com
               </a>
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+              <h2 className="title-font font-semibold text-yellow-500  tracking-widest text-xs mt-4">
                 PHONE
               </h2>
               <p className="leading-relaxed text-white">801-555-5555</p>
