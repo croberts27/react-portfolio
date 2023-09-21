f452b4ee2f71e9eef7fb31887ce64cbed981049a
Calvin Roberts <you@example.com>
2023-09-21 14:10:01 -0600

    updates allowing for one page at a time

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import About from "./components/About";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Footer from "./components/Footer";
@@ -8,14 +9,24 @@ import Blogs from "./components/Blog";
 
 function App() {
   return (
-    <main>
-      <Navbar />
-      <About />
-      <Projects />
-      <Skills />
-      <Blogs />
-      <Footer />
-    </main>
+    <Router>
+      {" "}
+      {/* Wrap entire app with BrowserRouter */}
+      <div>
+        <Navbar />
+        <Switch>
+          {" "}
+          {/* Use Switch to render only the first matching route */}
+          <Route path="/about" component={About} />{" "}
+          {/* Define a route for the About page */}
+          <Route path="/projects" component={Projects} />
+          <Route path="/skills" component={Skills} />
+          <Route path="/blogs" component={Blogs} />
+          <Route path="/" component={About} /> {/* Default homepage */}
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,56 +1,41 @@
-import { ArrowRightIcon } from "@heroicons/react/solid";
+// Navbar.jsx
+import { Link } from "react-router-dom"; // Import Link from React Router
 import React from "react";
 
 function Navbar() {
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 hover:text-yellow text-xl">
-            {/* <img className="h-20 w-22 pb-3 mb-2 rounded-sm" src="favicon.ico" /> */}
-            Home
-          </a>
-        </a>
-
+        <Link to="/" className="title-font font-medium text-white mb-4 md:mb-0">
+          Home
+        </Link>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-yellow	flex flex-wrap items-center text-lg justify-center">
-          <a
-            href="#about"
+          <Link
+            to="/about"
             className="mr-5 text-white rounded-md  hover:text-yellow"
           >
             About Me
-          </a>
-          <a
-            href="#projects"
+          </Link>
+          <Link
+            to="/projects"
             className="mr-5 text-white rounded-md  hover:text-yellow"
           >
             Portfolio
-          </a>
-          <a
-            href="#blogs"
+          </Link>
+          <Link
+            to="/blogs"
             className="mr-5 text-white rounded-md  hover:text-yellow"
           >
             Blog Posts
-          </a>
-          <a
-            href="#skills"
+          </Link>
+          <Link
+            to="/skills"
             className="mr-5 text-white rounded-md  hover:text-yellow"
           >
             Skills
-          </a>
-          <a
-            href="#footer"
-            className="mr-5 text-white rounded-md  hover:text-yellow"
-          >
-            Contact Me
-          </a>
+          </Link>
         </nav>
-        {/* <a
-          href="#contact"
-          className="inline-flex items-center bg-white border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
-        >
-          Hire Me
-          <ArrowRightIcon className="w-4 h-4 ml-1" />
-        </a> */}
+        {/* ... */}
       </div>
     </header>
   );
